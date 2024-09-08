923ee2e8412244615227f91a630662e624ff2caf
Reymundo Figueroa <reymundo_124@hotmail.com>
2024-09-08 02:57:54 -0600

    seccion de menu completada

---
+++ data.js
@@ -0,0 +1,114 @@
+const menu = [
+    {
+      id: 1,
+      name: 'Tacos Dorados',
+      price: [5, 10],
+      img: './assets/JPG/tacos-dorados.jpg',
+      flavors: ['Carne', 'Frijol', 'Papas', 'Rajas', 'Requesón', 'Picadillo']
+    },
+    {
+      id: 2,
+      name: 'Sopes',
+      price: [13, 16],
+      img: './assets/JPG/sopes.jpg',
+      flavors: ['Carne', 'Frijol', 'Papas', 'Rajas', 'Requesón', 'Picadillo']
+    },
+    {
+      id: 3,
+      name: 'Pellizcadas',
+      price: [20],
+      img: './assets/JPG/pellizcadas.jpg',
+      flavors: ['Bistec', 'Adobada', 'Lengua', 'Chicharrón', 'Costilla', 'Chorizo', 'Queso']
+    },
+    {
+      id: 4,
+      name: 'Pozole',
+      price: [25,35,45,55], // checar con manager precios 
+      img: './assets/JPG/pozole.jpg',
+      flavors: null
+    },
+    {
+      id: 5,
+      name: 'Flautas',
+      price: [20],
+      img: './assets/JPG/flautas.jpg',
+      flavors: ['Carne']
+    },
+    {
+      id: 6,
+      name: 'Quesadillas',
+      price: [20,25],
+      img: './assets/JPG/quesadillas.jpg',
+      flavors: ['Carne', 'Picadillo', 'Champiñones', 'Rajas', 'Bistec']
+    },
+    {
+      id: 7,
+      name: 'Taco de Guisado',
+      price: [20],
+      img: './assets/JPG/tacos-de-guisado.jpg',
+      flavors: ['Bistec', 'Adobada', 'Lengua', 'Chicharrón', 'Costilla', 'Chorizo', 'Queso']
+    },
+    {
+      id: 8,
+      name: 'Huaraches',
+      price: [55],
+      img: './assets/JPG/huaraches.jpg',
+      flavors: ['Bistec', 'Adobada', 'Lengua', 'Chicharrón', 'Costilla', 'Chorizo']
+    },
+    {
+      id: 9,
+      name: 'Gorditas',
+      price: [7],
+      img: './assets/JPG/gorditas.jpg',
+      flavors: ['De masa Sencilla']
+    },
+    {
+      id: 10,
+      name: 'Enchiladas',
+      price: [10,12],
+      img: './assets/JPG/enchiladas.jpg',
+      flavors: ['Carne', 'Queso']
+    },
+    {
+      id: 11,
+      name: 'Plátano Macho Frito',
+      price: [25],
+      img: './assets/JPG/platano-macho.jpg',
+      flavors: ['Lechera', 'Mermelada', 'Cajeta']
+    },
+    {
+      id: 12,
+      name: 'Arroz con Leche',
+      price: [15],
+      img: './assets/JPG/arroz-con-leche.jpg',
+      flavors: null
+    },
+    {
+      id: 13,
+      name: 'Flan',
+      price: [15],
+      img: './assets/JPG/flan.jpg',
+      flavors: null
+    },
+    {
+      id: 14,
+      name: 'Agua Fresca',
+      price: [16,25],
+      img: './assets/JPG/agua-fresca.jpg',
+      flavors: ['Sabor del Día']
+    },
+    {
+      id: 15,
+      name: 'Champurrado',
+      price: [18, 35], // checar precios con manager
+      img: './assets/JPG/atole.jpg',
+      flavors: null
+    },
+    {
+      id: 16,
+      name: 'Refrescos',
+      price: [20],
+      img: './assets/JPG/refrescos.jpg',
+      flavors: ['Coca Cola', 'Fresca', 'Sprite', 'Fanta', 'Sidral']
+    },
+]
---
+++ index.js
@@ -0,0 +1,50 @@
+function createMenuCard(menuItem) {
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add('card');
+
+  const leftSide = document.createElement('div');
+  leftSide.classList.add('card-left-side');
+
+  const nameParagraph = document.createElement('p');
+  nameParagraph.textContent = menuItem.name;
+
+  const flavorsList = document.createElement('ul');
+  flavorsList.classList.add('horizontal');
+  if (menuItem.flavors) {
+    menuItem.flavors.forEach(flavor => {
+      const flavorItem = document.createElement('li');
+      flavorItem.textContent = flavor;
+      flavorsList.appendChild(flavorItem);
+    });
+  } 
+
+  const priceSpan = document.createElement('span');
+  priceSpan.textContent = `MXN $${menuItem.price[0]}.00`;
+
+  leftSide.appendChild(nameParagraph);
+  leftSide.appendChild(flavorsList);
+  leftSide.appendChild(priceSpan);
+
+  const rightSide = document.createElement('div');
+  rightSide.classList.add('card-right-side');
+
+  const image = document.createElement('img');
+  image.src = menuItem.img;
+  image.alt = `imagen de ${menuItem.name}`;
+
+  rightSide.appendChild(image);
+
+  cardContainer.appendChild(leftSide);
+  cardContainer.appendChild(rightSide);
+
+  return cardContainer;
+}
+
+// Ejemplo de uso:
+
+
+const menuContainer = document.getElementById('cards-menu-container'); // Asegúrate de tener un elemento con el ID 'menu-container' en tu HTML
+menu.forEach(item => {
+  const card = createMenuCard(item);
+  menuContainer.appendChild(card);
+});
